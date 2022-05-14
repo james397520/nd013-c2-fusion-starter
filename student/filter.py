31433bc8890c48597aaa7e879d6ac474093eddb6
james.chan <chan.jamesyc@inventec.com>
2022-05-14 17:04:07 +0800

    feat(final): completed section 1: Single Target Tracking

--- student/filter.py
+++ student/filter.py
@@ -24,14 +24,21 @@ import misc.params as params
 class Filter:
     '''Kalman filter class'''
     def __init__(self):
-        pass
+        self.dim_state = params.dim_state # 6
+        self.dt = params.dt
+        self.q = params.q
 
     def F(self):
         ############
         # TODO Step 1: implement and return system matrix F
         ############
+        dt = self.dt
+        F = np.matrix(np.eye(6))
+        F[0, 3] = dt
+        F[1, 4] = dt
+        F[2, 5] = dt
 
-        return 0
+        return F
         
         ############
         # END student code
@@ -41,8 +48,18 @@ class Filter:
         ############
         # TODO Step 1: implement and return process noise covariance Q
         ############
+        dt = self.dt
+        q = self.q
+        q3 = q * np.power(dt, 3) / 3
+        q2 = q * np.power(dt, 2) / 2
+        q1 = q * dt
 
-        return 0
+        return np.matrix([[q3, 0, 0, q2, 0, 0],
+                          [0, q3, 0, 0, q2, 0],
+                          [0, 0, q3, 0, 0, q2],
+                          [q2, 0, 0, q1, 0, 0],
+                          [0, q2, 0, 0, q1, 0],
+                          [0, 0, q2, 0, 0, q1]])
         
         ############
         # END student code
@@ -52,8 +69,13 @@ class Filter:
         ############
         # TODO Step 1: predict state x and estimation error covariance P to next timestep, save x and P in track
         ############
+        
+        F = self.F()
+        x = F * track.x
+        P = F * track.P * F.transpose() + self.Q()
 
-        pass
+        track.set_x(x)
+        track.set_P(P)
         
         ############
         # END student code
@@ -63,10 +85,24 @@ class Filter:
         ############
         # TODO Step 1: update state x and covariance P with associated measurement, save x and P in track
         ############
-        
+
+        # Kalman update function need result is Gaussian distribution,
+        # so using Jacobian H, but calculate gamma we can just use h(x).
+        gamma = self.gamma(track, meas)
+        H = meas.sensor.get_H(track.x) # Jacobian H
+        S = self.S(track, meas, H) # covariance of residual
+        K = track.P * H.transpose() * np.linalg.inv(S) # Kalman gain
+
+        x = track.x + K * gamma # state update
+        I = np.identity(self.dim_state)
+        P = (I - K * H) * track.P # covariance update
+
+        track.set_x(x)
+        track.set_P(P)
+
         ############
         # END student code
-        ############ 
+        ############
         track.update_attributes(meas)
     
     def gamma(self, track, meas):
@@ -74,7 +110,7 @@ class Filter:
         # TODO Step 1: calculate and return residual gamma
         ############
 
-        return 0
+        return meas.z - meas.sensor.get_hx(track.x)
         
         ############
         # END student code
@@ -85,7 +121,7 @@ class Filter:
         # TODO Step 1: calculate and return covariance of residual S
         ############
 
-        return 0
+        return H * track.P * H.transpose() + meas.R
         
         ############
         # END student code
